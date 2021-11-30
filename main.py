f0cfe511390f7fccd092f1f83faca4bedf0535f7
Confusion-ymc <1107238486@qq.com>
2021-12-01 00:55:28 +0800

    升级pip

--- main.py
+++ main.py
@@ -31,45 +31,72 @@ def check_ip():
         raise Exception(f'查询IP失败！ {html_data}')
 
 
-class AliUpdater:
-    def __init__(self, update_record, access_key=None, access_secret=None):
-        if not update_record:
-            raise Exception('未设置 update_record')
-        self.runner_name = 'aliyun'
+# class AliUpdater:
+#     def __init__(self, update_record, access_key=None, access_secret=None):
+#         if not update_record:
+#             raise Exception('未设置 update_record')
+#         self.runner_name = 'aliyun'
+#         self.update_record = update_record
+#         self.credentials = AccessKeyCredential(access_key, access_secret)
+#         self.client = AcsClient(region_id='cn-chengdu', credential=self.credentials)
+#
+#     def check_records(self, name, ip):
+#         request = DescribeDomainRecordsRequest()
+#         request.set_accept_format('json')
+#         request.set_DomainName("ymccc.xin")
+#         response = self.client.do_action_with_exception(request)
+#         json_res = json.loads(response)
+#         for item in json_res['DomainRecords']['Record']:
+#             if item['RR'] + '.' + item['DomainName'] == name and item['Type'] == 'A':
+#                 if item['Value'] != ip:
+#                     return item['RecordId'], item['Value']
+#                 else:
+#                     return None, None
+#         log.info(f'未找到记录 {name}')
+#
+#     def set_records(self, record_id, name, ip):
+#         request = CommonRequest()
+#         request.set_accept_format('json')
+#         request.set_domain('alidns.cn-beijing.aliyuncs.com')
+#         request.set_method('POST')
+#         request.set_protocol_type('https')  # https | http
+#         request.set_version('2015-01-09')
+#         request.set_action_name('UpdateDomainRecord')
+#         request.add_query_param('RecordId', record_id)  # record_item['RecordId']
+#         request.add_query_param('RR', name)
+#         request.add_query_param('Type', "A")
+#         request.add_query_param('Value', ip)
+#
+#         response = self.client.do_action_with_exception(request)
+#         json_res = json.loads(response)
+#         return json_res
+#
+#     def update(self, name, ip):
+#         record_id, old_ip = self.check_records(name, ip)
+#         if record_id:
+#             self.set_records(record_id=record_id, name=name, ip=ip)
+#             log.info(f'更新成功 {name} ---> {ip}  （原 {old_ip}）')
+#             send_email.send(f'更新成功 {name} ---> {ip}  （原 {old_ip}）')
+#         else:
+#             pass
+#             # log.info(f'记录正确,无需修改 {name} ---> {ip}')
+#
+#     def run(self):
+#         log.info(f'-----【{self.runner_name}】 开始执行 -----')
+#         try:
+#             now_ip = check_ip()
+#             self.update(self.update_record, now_ip)
+#         except Exception as e:
+#             log.error(f'更新失败 {e}')
+
+
+class CFUpdater:
+    def __init__(self, update_record, email, api_key, zone_id):
+        self.runner_name = 'cloudflare'
+        self.email = email
+        self.api_key = api_key
+        self.zone_id = zone_id
         self.update_record = update_record
-        self.credentials = AccessKeyCredential(access_key, access_secret)
-        self.client = AcsClient(region_id='cn-chengdu', credential=self.credentials)
-
-    def check_records(self, name, ip):
-        request = DescribeDomainRecordsRequest()
-        request.set_accept_format('json')
-        request.set_DomainName("ymccc.xin")
-        response = self.client.do_action_with_exception(request)
-        json_res = json.loads(response)
-        for item in json_res['DomainRecords']['Record']:
-            if item['RR'] + '.' + item['DomainName'] == name and item['Type'] == 'A':
-                if item['Value'] != ip:
-                    return item['RecordId'], item['Value']
-                else:
-                    return None, None
-        log.info(f'未找到记录 {name}')
-
-    def set_records(self, record_id, name, ip):
-        request = CommonRequest()
-        request.set_accept_format('json')
-        request.set_domain('alidns.cn-beijing.aliyuncs.com')
-        request.set_method('POST')
-        request.set_protocol_type('https')  # https | http
-        request.set_version('2015-01-09')
-        request.set_action_name('UpdateDomainRecord')
-        request.add_query_param('RecordId', record_id)  # record_item['RecordId']
-        request.add_query_param('RR', name)
-        request.add_query_param('Type', "A")
-        request.add_query_param('Value', ip)
-
-        response = self.client.do_action_with_exception(request)
-        json_res = json.loads(response)
-        return json_res
 
     def update(self, name, ip):
         record_id, old_ip = self.check_records(name, ip)
@@ -80,23 +107,6 @@ class AliUpdater:
         else:
             pass
             # log.info(f'记录正确,无需修改 {name} ---> {ip}')
-
-    def run(self):
-        log.info(f'-----【{self.runner_name}】 开始执行 -----')
-        try:
-            now_ip = check_ip()
-            self.update(self.update_record, now_ip)
-        except Exception as e:
-            log.error(f'更新失败 {e}')
-
-
-class CFUpdater(AliUpdater):
-    def __init__(self, update_record, email, api_key, zone_id):
-        super(CFUpdater, self).__init__(update_record)
-        self.runner_name = 'cloudflare'
-        self.email = email
-        self.api_key = api_key
-        self.zone_id = zone_id
 
     def check_records(self, name, ip):
         headers = {
@@ -137,6 +147,14 @@ class CFUpdater(AliUpdater):
             headers=headers, json=data, timeout=20)
         return response.json()
 
+    def run(self):
+        log.info(f'-----【{self.runner_name}】 开始执行 -----')
+        try:
+            now_ip = check_ip()
+            self.update(self.update_record, now_ip)
+        except Exception as e:
+            log.error(f'更新失败 {e}')
+
 
 if __name__ == '__main__':
     config_path = 'config'
@@ -146,15 +164,14 @@ if __name__ == '__main__':
     record = conf.get('SET', 'update_record')
     check_sleep = conf.getint('SET', 'sleep_time')
 
-    if conf.get('SET', 'client_type') == 'CF':
-        cf_email = conf.get('CF', 'email')
-        cf_api_key = conf.get('CF', 'api_key')
-        cf_zone_id = conf.get('CF', 'zone_id')
-        set_client = CFUpdater(record, cf_email, cf_api_key, cf_zone_id)
-    else:
-        ali_key = conf.get('Ali', 'access_key')
-        ali_secret = conf.get('Ali', 'access_key_secret')
-        set_client = AliUpdater(record, ali_key, ali_secret)
+    cf_email = conf.get('CF', 'email')
+    cf_api_key = conf.get('CF', 'api_key')
+    cf_zone_id = conf.get('CF', 'zone_id')
+    set_client = CFUpdater(record, cf_email, cf_api_key, cf_zone_id)
+    # else:
+    #     ali_key = conf.get('Ali', 'access_key')
+    #     ali_secret = conf.get('Ali', 'access_key_secret')
+    #     set_client = AliUpdater(record, ali_key, ali_secret)
     while True:
         set_client.run()
         time.sleep(check_sleep)
